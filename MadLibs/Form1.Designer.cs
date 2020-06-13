60cddea276d042675dd785be012810b98dd69733
Drew Johnson <andrjohn746@ed.amdsb.ca>
2020-06-13 19:48:36 -0400

    sick haircut dude

--- MadLibs/Form1.Designer.cs
+++ MadLibs/Form1.Designer.cs
@@ -34,6 +34,12 @@
             this.titleSmile = new System.Windows.Forms.Label();
             this.selectButton = new System.Windows.Forms.Button();
             this.quitButton = new System.Windows.Forms.Button();
+            this.mainButton = new System.Windows.Forms.Button();
+            this.blankButton = new System.Windows.Forms.Button();
+            this.loveButton = new System.Windows.Forms.Button();
+            this.turtleButton = new System.Windows.Forms.Button();
+            this.timeButton = new System.Windows.Forms.Button();
+            this.fortuneButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -41,7 +47,7 @@
             this.titleLabel.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLabel.Location = new System.Drawing.Point(12, 9);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(508, 98);
+            this.titleLabel.Size = new System.Drawing.Size(508, 137);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "Mad Libs";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -94,6 +100,7 @@
             this.selectButton.TabIndex = 4;
             this.selectButton.Text = "Select\r\nStory";
             this.selectButton.UseVisualStyleBackColor = false;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
             // 
             // quitButton
             // 
@@ -109,6 +116,98 @@
             this.quitButton.TabIndex = 5;
             this.quitButton.Text = "Quit";
             this.quitButton.UseVisualStyleBackColor = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // mainButton
+            // 
+            this.mainButton.BackColor = System.Drawing.Color.LightCoral;
+            this.mainButton.FlatAppearance.BorderSize = 0;
+            this.mainButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tomato;
+            this.mainButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mainButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.mainButton.Location = new System.Drawing.Point(290, 375);
+            this.mainButton.Name = "mainButton";
+            this.mainButton.Size = new System.Drawing.Size(153, 107);
+            this.mainButton.TabIndex = 7;
+            this.mainButton.Text = "Return to Main Menu";
+            this.mainButton.UseVisualStyleBackColor = false;
+            this.mainButton.Visible = false;
+            // 
+            // blankButton
+            // 
+            this.blankButton.BackColor = System.Drawing.Color.LightCyan;
+            this.blankButton.FlatAppearance.BorderSize = 0;
+            this.blankButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.blankButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.blankButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankButton.Location = new System.Drawing.Point(86, 375);
+            this.blankButton.Name = "blankButton";
+            this.blankButton.Size = new System.Drawing.Size(153, 107);
+            this.blankButton.TabIndex = 8;
+            this.blankButton.Text = "Story 5:\r\nBlankball";
+            this.blankButton.UseVisualStyleBackColor = false;
+            this.blankButton.Visible = false;
+            // 
+            // loveButton
+            // 
+            this.loveButton.BackColor = System.Drawing.Color.LightCyan;
+            this.loveButton.FlatAppearance.BorderSize = 0;
+            this.loveButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.loveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loveButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loveButton.Location = new System.Drawing.Point(290, 149);
+            this.loveButton.Name = "loveButton";
+            this.loveButton.Size = new System.Drawing.Size(153, 107);
+            this.loveButton.TabIndex = 10;
+            this.loveButton.Text = "Story 2:\r\nLove Letter";
+            this.loveButton.UseVisualStyleBackColor = false;
+            this.loveButton.Visible = false;
+            // 
+            // turtleButton
+            // 
+            this.turtleButton.BackColor = System.Drawing.Color.LightCyan;
+            this.turtleButton.FlatAppearance.BorderSize = 0;
+            this.turtleButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.turtleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.turtleButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turtleButton.Location = new System.Drawing.Point(86, 149);
+            this.turtleButton.Name = "turtleButton";
+            this.turtleButton.Size = new System.Drawing.Size(153, 107);
+            this.turtleButton.TabIndex = 11;
+            this.turtleButton.Text = "Story 1:\r\nRip-Off Ninja Turtles";
+            this.turtleButton.UseVisualStyleBackColor = false;
+            this.turtleButton.Visible = false;
+            // 
+            // timeButton
+            // 
+            this.timeButton.BackColor = System.Drawing.Color.LightCyan;
+            this.timeButton.FlatAppearance.BorderSize = 0;
+            this.timeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.timeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.timeButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeButton.Location = new System.Drawing.Point(86, 262);
+            this.timeButton.Name = "timeButton";
+            this.timeButton.Size = new System.Drawing.Size(153, 107);
+            this.timeButton.TabIndex = 12;
+            this.timeButton.Text = "Story 3:\r\n Once Upon a Time";
+            this.timeButton.UseVisualStyleBackColor = false;
+            this.timeButton.Visible = false;
+            // 
+            // fortuneButton
+            // 
+            this.fortuneButton.BackColor = System.Drawing.Color.LightCyan;
+            this.fortuneButton.FlatAppearance.BorderSize = 0;
+            this.fortuneButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSteelBlue;
+            this.fortuneButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fortuneButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fortuneButton.Location = new System.Drawing.Point(290, 262);
+            this.fortuneButton.Name = "fortuneButton";
+            this.fortuneButton.Size = new System.Drawing.Size(153, 107);
+            this.fortuneButton.TabIndex = 13;
+            this.fortuneButton.Text = "Story 4:\r\n Fortune Cookies";
+            this.fortuneButton.UseVisualStyleBackColor = false;
+            this.fortuneButton.Visible = false;
             // 
             // Form1
             // 
@@ -116,6 +215,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleTurquoise;
             this.ClientSize = new System.Drawing.Size(532, 503);
+            this.Controls.Add(this.fortuneButton);
+            this.Controls.Add(this.timeButton);
+            this.Controls.Add(this.turtleButton);
+            this.Controls.Add(this.loveButton);
+            this.Controls.Add(this.blankButton);
+            this.Controls.Add(this.mainButton);
             this.Controls.Add(this.quitButton);
             this.Controls.Add(this.selectButton);
             this.Controls.Add(this.randButton);
@@ -137,6 +242,12 @@
         private System.Windows.Forms.Button randButton;
         private System.Windows.Forms.Button selectButton;
         private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button mainButton;
+        private System.Windows.Forms.Button blankButton;
+        private System.Windows.Forms.Button loveButton;
+        private System.Windows.Forms.Button turtleButton;
+        private System.Windows.Forms.Button timeButton;
+        private System.Windows.Forms.Button fortuneButton;
     }
 }
 
--- MadLibs/Form1.cs
+++ MadLibs/Form1.cs
@@ -12,19 +12,85 @@ namespace MadLibs
 {
     public partial class Form1 : Form
     {
+        int randomNum = 0;
+        Random randGen = new Random();
+
         public Form1()
         {
+            
             InitializeComponent();
+        }
+
+        public void titleSetup()
+        {
+            titleSmile.Visible = true;
+            titleLabel.Visible = true;
+            titleLabel.Text = "Mad Libs";
+            subtitleLabel.Visible = true;
+            randButton.Visible = true;
+            selectButton.Visible = true;
+            quitButton.Visible = true;
+            randButton.Enabled = true;
+            selectButton.Enabled = true;
+            randButton.Enabled = true;
+        }
+        public void titleClear()
+        {
+            titleSmile.Visible = false;
+            titleLabel.Visible = false;
+            subtitleLabel.Visible = false;
+            randButton.Visible = false;
+            selectButton.Visible = false;
+            quitButton.Visible = false;
+            randButton.Enabled = false;
+            selectButton.Enabled = false;
+            randButton.Enabled = false;
+        }
+        public void selectSetup()
+        {
+            titleLabel.Visible = true;
+            titleLabel.Text = "Stages";
+            turtleButton.Visible = true;
+            loveButton.Visible = true;
+            timeButton.Visible = true;
+            fortuneButton.Visible = true;
+            blankButton.Visible = true;
+            mainButton.Visible = true;
+        }
+        public void selectClear()
+        {
+            titleLabel.Visible = false;
+            titleLabel.Text = "Mad Libs";
+            turtleButton.Visible = false;
+            loveButton.Visible = false;
+            timeButton.Visible = false;
+            fortuneButton.Visible = false;
+            blankButton.Visible = false;
+            mainButton.Visible = false;
         }
 
         private void randButton_Click(object sender, EventArgs e)
         {
+            titleClear();
 
+            randomNum = randGen.Next(1, 6);
+
+            // if (randomNum == 1){}
+            // if (randomNum == 2){}
+            // if (randomNum == 3){}
+            // if (randomNum == 4){}
+            // if (randomNum == 5){}
         }
 
-        private void titleClear()
+        private void selectButton_Click(object sender, EventArgs e)
         {
+            titleClear();
+            selectSetup();
+        }
 
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
         }
     }
 }
