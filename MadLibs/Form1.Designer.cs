da070b725ecb9f010cb5d3307b40e32e894e3fdb
Drew Johnson <andrjohn746@ed.amdsb.ca>
2020-06-15 11:51:19 -0400

    woah talk about lotsa changes

--- MadLibs/Form1.Designer.cs
+++ MadLibs/Form1.Designer.cs
@@ -40,6 +40,28 @@
             this.turtleButton = new System.Windows.Forms.Button();
             this.timeButton = new System.Windows.Forms.Button();
             this.fortuneButton = new System.Windows.Forms.Button();
+            this.warningPopUp = new System.Windows.Forms.Label();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.word1Label = new System.Windows.Forms.Label();
+            this.word1Box = new System.Windows.Forms.TextBox();
+            this.word5Label = new System.Windows.Forms.Label();
+            this.word10Label = new System.Windows.Forms.Label();
+            this.word9Label = new System.Windows.Forms.Label();
+            this.word8Label = new System.Windows.Forms.Label();
+            this.word7Label = new System.Windows.Forms.Label();
+            this.word6Label = new System.Windows.Forms.Label();
+            this.word4Label = new System.Windows.Forms.Label();
+            this.word3Label = new System.Windows.Forms.Label();
+            this.word2Label = new System.Windows.Forms.Label();
+            this.word9Box = new System.Windows.Forms.TextBox();
+            this.word8Box = new System.Windows.Forms.TextBox();
+            this.word7Box = new System.Windows.Forms.TextBox();
+            this.word6Box = new System.Windows.Forms.TextBox();
+            this.word5Box = new System.Windows.Forms.TextBox();
+            this.word4Box = new System.Windows.Forms.TextBox();
+            this.word3Box = new System.Windows.Forms.TextBox();
+            this.word2Box = new System.Windows.Forms.TextBox();
+            this.word10Box = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -133,6 +155,7 @@
             this.mainButton.Text = "Return to Main Menu";
             this.mainButton.UseVisualStyleBackColor = false;
             this.mainButton.Visible = false;
+            this.mainButton.Click += new System.EventHandler(this.mainButton_Click);
             // 
             // blankButton
             // 
@@ -148,6 +171,7 @@
             this.blankButton.Text = "Story 5:\r\nBlankball";
             this.blankButton.UseVisualStyleBackColor = false;
             this.blankButton.Visible = false;
+            this.blankButton.Click += new System.EventHandler(this.blankButton_Click_1);
             // 
             // loveButton
             // 
@@ -163,6 +187,7 @@
             this.loveButton.Text = "Story 2:\r\nLove Letter";
             this.loveButton.UseVisualStyleBackColor = false;
             this.loveButton.Visible = false;
+            this.loveButton.Click += new System.EventHandler(this.loveButton_Click);
             // 
             // turtleButton
             // 
@@ -178,6 +203,7 @@
             this.turtleButton.Text = "Story 1:\r\nRip-Off Ninja Turtles";
             this.turtleButton.UseVisualStyleBackColor = false;
             this.turtleButton.Visible = false;
+            this.turtleButton.Click += new System.EventHandler(this.turtleButton_Click_1);
             // 
             // timeButton
             // 
@@ -193,6 +219,7 @@
             this.timeButton.Text = "Story 3:\r\n Once Upon a Time";
             this.timeButton.UseVisualStyleBackColor = false;
             this.timeButton.Visible = false;
+            this.timeButton.Click += new System.EventHandler(this.timeButton_Click);
             // 
             // fortuneButton
             // 
@@ -208,6 +235,236 @@
             this.fortuneButton.Text = "Story 4:\r\n Fortune Cookies";
             this.fortuneButton.UseVisualStyleBackColor = false;
             this.fortuneButton.Visible = false;
+            this.fortuneButton.Click += new System.EventHandler(this.fortuneButton_Click);
+            // 
+            // warningPopUp
+            // 
+            this.warningPopUp.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.warningPopUp.Location = new System.Drawing.Point(13, 9);
+            this.warningPopUp.Name = "warningPopUp";
+            this.warningPopUp.Size = new System.Drawing.Size(507, 360);
+            this.warningPopUp.TabIndex = 14;
+            this.warningPopUp.Text = "Uh oh! An error occurred!\r\n\r\nPlease make sure all boxes are filled before continu" +
+    "ing to the story.";
+            this.warningPopUp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.warningPopUp.Visible = false;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.BackColor = System.Drawing.Color.LightGreen;
+            this.confirmButton.FlatAppearance.BorderSize = 0;
+            this.confirmButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.confirmButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.confirmButton.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.confirmButton.Location = new System.Drawing.Point(86, 375);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(153, 107);
+            this.confirmButton.TabIndex = 15;
+            this.confirmButton.Text = "Confirm my Choices\r\n";
+            this.confirmButton.UseVisualStyleBackColor = false;
+            this.confirmButton.Visible = false;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // word1Label
+            // 
+            this.word1Label.BackColor = System.Drawing.Color.Transparent;
+            this.word1Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word1Label.Location = new System.Drawing.Point(16, 37);
+            this.word1Label.Name = "word1Label";
+            this.word1Label.Size = new System.Drawing.Size(223, 23);
+            this.word1Label.TabIndex = 16;
+            this.word1Label.Text = "label1";
+            this.word1Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word1Label.Visible = false;
+            // 
+            // word1Box
+            // 
+            this.word1Box.Location = new System.Drawing.Point(245, 40);
+            this.word1Box.Name = "word1Box";
+            this.word1Box.Size = new System.Drawing.Size(232, 22);
+            this.word1Box.TabIndex = 17;
+            this.word1Box.Visible = false;
+            // 
+            // word5Label
+            // 
+            this.word5Label.BackColor = System.Drawing.Color.Transparent;
+            this.word5Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word5Label.Location = new System.Drawing.Point(16, 160);
+            this.word5Label.Name = "word5Label";
+            this.word5Label.Size = new System.Drawing.Size(223, 23);
+            this.word5Label.TabIndex = 19;
+            this.word5Label.Text = "label1";
+            this.word5Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word5Label.Visible = false;
+            // 
+            // word10Label
+            // 
+            this.word10Label.BackColor = System.Drawing.Color.Transparent;
+            this.word10Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word10Label.Location = new System.Drawing.Point(16, 327);
+            this.word10Label.Name = "word10Label";
+            this.word10Label.Size = new System.Drawing.Size(223, 23);
+            this.word10Label.TabIndex = 20;
+            this.word10Label.Text = "label1";
+            this.word10Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word10Label.Visible = false;
+            // 
+            // word9Label
+            // 
+            this.word9Label.BackColor = System.Drawing.Color.Transparent;
+            this.word9Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word9Label.Location = new System.Drawing.Point(16, 291);
+            this.word9Label.Name = "word9Label";
+            this.word9Label.Size = new System.Drawing.Size(223, 23);
+            this.word9Label.TabIndex = 21;
+            this.word9Label.Text = "label1";
+            this.word9Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word9Label.Visible = false;
+            // 
+            // word8Label
+            // 
+            this.word8Label.BackColor = System.Drawing.Color.Transparent;
+            this.word8Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word8Label.Location = new System.Drawing.Point(16, 259);
+            this.word8Label.Name = "word8Label";
+            this.word8Label.Size = new System.Drawing.Size(223, 23);
+            this.word8Label.TabIndex = 22;
+            this.word8Label.Text = "label1";
+            this.word8Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word8Label.Visible = false;
+            // 
+            // word7Label
+            // 
+            this.word7Label.BackColor = System.Drawing.Color.Transparent;
+            this.word7Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word7Label.Location = new System.Drawing.Point(16, 224);
+            this.word7Label.Name = "word7Label";
+            this.word7Label.Size = new System.Drawing.Size(223, 23);
+            this.word7Label.TabIndex = 23;
+            this.word7Label.Text = "label1";
+            this.word7Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word7Label.Visible = false;
+            // 
+            // word6Label
+            // 
+            this.word6Label.BackColor = System.Drawing.Color.Transparent;
+            this.word6Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word6Label.Location = new System.Drawing.Point(16, 190);
+            this.word6Label.Name = "word6Label";
+            this.word6Label.Size = new System.Drawing.Size(223, 23);
+            this.word6Label.TabIndex = 24;
+            this.word6Label.Text = "label1";
+            this.word6Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word6Label.Visible = false;
+            // 
+            // word4Label
+            // 
+            this.word4Label.BackColor = System.Drawing.Color.Transparent;
+            this.word4Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word4Label.Location = new System.Drawing.Point(16, 130);
+            this.word4Label.Name = "word4Label";
+            this.word4Label.Size = new System.Drawing.Size(223, 23);
+            this.word4Label.TabIndex = 25;
+            this.word4Label.Text = "label1";
+            this.word4Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word4Label.Visible = false;
+            // 
+            // word3Label
+            // 
+            this.word3Label.BackColor = System.Drawing.Color.Transparent;
+            this.word3Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word3Label.Location = new System.Drawing.Point(16, 100);
+            this.word3Label.Name = "word3Label";
+            this.word3Label.Size = new System.Drawing.Size(223, 23);
+            this.word3Label.TabIndex = 26;
+            this.word3Label.Text = "label1";
+            this.word3Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word3Label.Visible = false;
+            // 
+            // word2Label
+            // 
+            this.word2Label.BackColor = System.Drawing.Color.Transparent;
+            this.word2Label.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word2Label.Location = new System.Drawing.Point(16, 69);
+            this.word2Label.Name = "word2Label";
+            this.word2Label.Size = new System.Drawing.Size(223, 23);
+            this.word2Label.TabIndex = 27;
+            this.word2Label.Text = "label1";
+            this.word2Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.word2Label.Visible = false;
+            // 
+            // word9Box
+            // 
+            this.word9Box.Location = new System.Drawing.Point(245, 292);
+            this.word9Box.Name = "word9Box";
+            this.word9Box.Size = new System.Drawing.Size(232, 22);
+            this.word9Box.TabIndex = 28;
+            this.word9Box.Visible = false;
+            // 
+            // word8Box
+            // 
+            this.word8Box.Location = new System.Drawing.Point(245, 259);
+            this.word8Box.Name = "word8Box";
+            this.word8Box.Size = new System.Drawing.Size(232, 22);
+            this.word8Box.TabIndex = 29;
+            this.word8Box.Visible = false;
+            // 
+            // word7Box
+            // 
+            this.word7Box.Location = new System.Drawing.Point(245, 224);
+            this.word7Box.Name = "word7Box";
+            this.word7Box.Size = new System.Drawing.Size(232, 22);
+            this.word7Box.TabIndex = 30;
+            this.word7Box.Visible = false;
+            // 
+            // word6Box
+            // 
+            this.word6Box.Location = new System.Drawing.Point(245, 193);
+            this.word6Box.Name = "word6Box";
+            this.word6Box.Size = new System.Drawing.Size(232, 22);
+            this.word6Box.TabIndex = 31;
+            this.word6Box.Visible = false;
+            // 
+            // word5Box
+            // 
+            this.word5Box.Location = new System.Drawing.Point(245, 161);
+            this.word5Box.Name = "word5Box";
+            this.word5Box.Size = new System.Drawing.Size(232, 22);
+            this.word5Box.TabIndex = 32;
+            this.word5Box.Visible = false;
+            // 
+            // word4Box
+            // 
+            this.word4Box.Location = new System.Drawing.Point(245, 133);
+            this.word4Box.Name = "word4Box";
+            this.word4Box.Size = new System.Drawing.Size(232, 22);
+            this.word4Box.TabIndex = 33;
+            this.word4Box.Visible = false;
+            // 
+            // word3Box
+            // 
+            this.word3Box.Location = new System.Drawing.Point(245, 100);
+            this.word3Box.Name = "word3Box";
+            this.word3Box.Size = new System.Drawing.Size(232, 22);
+            this.word3Box.TabIndex = 34;
+            this.word3Box.Visible = false;
+            // 
+            // word2Box
+            // 
+            this.word2Box.Location = new System.Drawing.Point(245, 68);
+            this.word2Box.Name = "word2Box";
+            this.word2Box.Size = new System.Drawing.Size(232, 22);
+            this.word2Box.TabIndex = 35;
+            this.word2Box.Visible = false;
+            // 
+            // word10Box
+            // 
+            this.word10Box.Location = new System.Drawing.Point(245, 330);
+            this.word10Box.Name = "word10Box";
+            this.word10Box.Size = new System.Drawing.Size(232, 22);
+            this.word10Box.TabIndex = 37;
+            this.word10Box.Visible = false;
             // 
             // Form1
             // 
@@ -215,6 +472,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleTurquoise;
             this.ClientSize = new System.Drawing.Size(532, 503);
+            this.Controls.Add(this.warningPopUp);
+            this.Controls.Add(this.word10Box);
+            this.Controls.Add(this.word2Box);
+            this.Controls.Add(this.word3Box);
+            this.Controls.Add(this.word4Box);
+            this.Controls.Add(this.word5Box);
+            this.Controls.Add(this.word6Box);
+            this.Controls.Add(this.word7Box);
+            this.Controls.Add(this.word8Box);
+            this.Controls.Add(this.word9Box);
+            this.Controls.Add(this.word2Label);
+            this.Controls.Add(this.word3Label);
+            this.Controls.Add(this.word4Label);
+            this.Controls.Add(this.word6Label);
+            this.Controls.Add(this.word7Label);
+            this.Controls.Add(this.word8Label);
+            this.Controls.Add(this.word9Label);
+            this.Controls.Add(this.word10Label);
+            this.Controls.Add(this.word5Label);
+            this.Controls.Add(this.word1Box);
+            this.Controls.Add(this.word1Label);
             this.Controls.Add(this.fortuneButton);
             this.Controls.Add(this.timeButton);
             this.Controls.Add(this.turtleButton);
@@ -227,10 +505,12 @@
             this.Controls.Add(this.titleSmile);
             this.Controls.Add(this.subtitleLabel);
             this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.confirmButton);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -248,6 +528,28 @@
         private System.Windows.Forms.Button turtleButton;
         private System.Windows.Forms.Button timeButton;
         private System.Windows.Forms.Button fortuneButton;
+        private System.Windows.Forms.Label warningPopUp;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Label word1Label;
+        private System.Windows.Forms.TextBox word1Box;
+        private System.Windows.Forms.Label word5Label;
+        private System.Windows.Forms.Label word10Label;
+        private System.Windows.Forms.Label word9Label;
+        private System.Windows.Forms.Label word8Label;
+        private System.Windows.Forms.Label word7Label;
+        private System.Windows.Forms.Label word6Label;
+        private System.Windows.Forms.Label word4Label;
+        private System.Windows.Forms.Label word3Label;
+        private System.Windows.Forms.Label word2Label;
+        private System.Windows.Forms.TextBox word9Box;
+        private System.Windows.Forms.TextBox word8Box;
+        private System.Windows.Forms.TextBox word7Box;
+        private System.Windows.Forms.TextBox word6Box;
+        private System.Windows.Forms.TextBox word5Box;
+        private System.Windows.Forms.TextBox word4Box;
+        private System.Windows.Forms.TextBox word3Box;
+        private System.Windows.Forms.TextBox word2Box;
+        private System.Windows.Forms.TextBox word10Box;
     }
 }
 
--- MadLibs/Form1.cs
+++ MadLibs/Form1.cs
@@ -15,9 +15,13 @@ namespace MadLibs
         int randomNum = 0;
         Random randGen = new Random();
 
+        string word1, word2, word3, word4, word5, word6, word7, word8, word9, word10;
+
+        int popUp = 0;
+
         public Form1()
         {
-            
+
             InitializeComponent();
         }
 
@@ -56,6 +60,7 @@ namespace MadLibs
             fortuneButton.Visible = true;
             blankButton.Visible = true;
             mainButton.Visible = true;
+            blankButton.Enabled = true;
         }
         public void selectClear()
         {
@@ -67,7 +72,109 @@ namespace MadLibs
             fortuneButton.Visible = false;
             blankButton.Visible = false;
             mainButton.Visible = false;
+            blankButton.Enabled = false;
         }
+        public void wordSetup()
+        {
+            mainButton.Visible = true;
+            confirmButton.Visible = true;
+            confirmButton.Enabled = true;
+
+            word1Label.Visible = true;
+            word1Box.Visible = true;
+            word2Label.Visible = true;
+            word2Box.Visible = true;
+            word3Label.Visible = true;
+            word3Box.Visible = true;
+            word4Label.Visible = true;
+            word4Box.Visible = true;
+            word5Label.Visible = true;
+            word5Box.Visible = true;
+            word6Label.Visible = true;
+            word6Box.Visible = true;
+            word7Label.Visible = true;
+            word7Box.Visible = true;
+            word8Label.Visible = true;
+            word8Box.Visible = true;
+            word9Label.Visible = true;
+            word9Box.Visible = true;
+            word10Label.Visible = true;
+            word10Box.Visible = true;
+        }
+        public void wordClear()
+        {
+            mainButton.Visible = false;
+            confirmButton.Visible = false;
+            confirmButton.Enabled = false;
+
+            word1Label.Visible = false;
+            word1Box.Visible = false;
+            word2Label.Visible = false;
+            word2Box.Visible = false;
+            word3Label.Visible = false;
+            word3Box.Visible = false;
+            word4Label.Visible = false;
+            word4Box.Visible = false;
+            word5Label.Visible = false;
+            word5Box.Visible = false;
+            word6Label.Visible = false;
+            word6Box.Visible = false;
+            word7Label.Visible = false;
+            word7Box.Visible = false;
+            word8Label.Visible = false;
+            word8Box.Visible = false;
+            word9Label.Visible = false;
+            word9Box.Visible = false;
+            word10Label.Visible = false;
+            word10Box.Visible = false;
+        }
+        public void warningUp()
+        {
+            word1Label.Visible = false;
+            word1Box.Visible = false;
+            word2Label.Visible = false;
+            word2Box.Visible = false;
+            word3Label.Visible = false;
+            word3Box.Visible = false;
+            word4Label.Visible = false;
+            word4Box.Visible = false;
+            word5Label.Visible = false;
+            word5Box.Visible = false;
+            word6Label.Visible = false;
+            word6Box.Visible = false;
+            word7Label.Visible = false;
+            word7Box.Visible = false;
+            word8Label.Visible = false;
+            word8Box.Visible = false;
+            word9Label.Visible = false;
+            word9Box.Visible = false;
+            word10Label.Visible = false;
+            word10Box.Visible = false;
+        }
+        public void warningDown()
+        {
+            word1Label.Visible = true;
+            word1Box.Visible = true;
+            word2Label.Visible = true;
+            word2Box.Visible = true;
+            word3Label.Visible = true;
+            word3Box.Visible = true;
+            word4Label.Visible = true;
+            word4Box.Visible = true;
+            word5Label.Visible = true;
+            word5Box.Visible = true;
+            word6Label.Visible = true;
+            word6Box.Visible = true;
+            word7Label.Visible = true;
+            word7Box.Visible = true;
+            word8Label.Visible = true;
+            word8Box.Visible = true;
+            word9Label.Visible = true;
+            word9Box.Visible = true;
+            word10Label.Visible = true;
+            word10Box.Visible = true;
+        }
+
 
         private void randButton_Click(object sender, EventArgs e)
         {
@@ -82,15 +189,87 @@ namespace MadLibs
             // if (randomNum == 5){}
         }
 
+        
+        private void turtleButton_Click_1(object sender, EventArgs e)
+        {
+            selectClear();
+            wordSetup();
+        }
+        private void loveButton_Click(object sender, EventArgs e)
+        {
+            selectClear();
+            wordSetup();
+        }
+        private void timeButton_Click(object sender, EventArgs e)
+        {
+            selectClear();
+            wordSetup();
+        }
+        private void fortuneButton_Click(object sender, EventArgs e)
+        {
+            selectClear();
+            wordSetup();
+        }
+        private void blankButton_Click_1(object sender, EventArgs e)
+        {
+            selectClear();
+            wordSetup();
+        }
+
+
         private void selectButton_Click(object sender, EventArgs e)
         {
             titleClear();
             selectSetup();
         }
-
         private void quitButton_Click(object sender, EventArgs e)
         {
             Environment.Exit(1);
+        }
+        private void mainButton_Click(object sender, EventArgs e)
+        {
+            wordClear();
+            selectClear();
+            titleSetup();
+        }
+
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (popUp == 0)
+            {
+                if (word1Box.Text==null||word1Box.Text == ""|| word2Box.Text == null || word2Box.Text == "" || word3Box.Text == null || word3Box.Text == "" || word4Box.Text == null || word4Box.Text == "" || word5Box.Text == null || word5Box.Text == "" || word6Box.Text == null || word6Box.Text == "" || word7Box.Text == null || word7Box.Text == "" || word8Box.Text == null || word8Box.Text == "" || word9Box.Text == null || word9Box.Text == "" || word10Box.Text == null || word10Box.Text == "")
+                {
+                    warningUp();
+                    popUp = 1;
+                    warningPopUp.Visible = true;
+                    confirmButton.Text = "Review my Choices";
+                    mainButton.Visible = false;
+                    confirmButton.Location = new Point(confirmButton.Location.X + 77, confirmButton.Location.Y);                    
+                }
+                else
+                {
+                    word1 = Convert.ToString(word1Box.Text);
+                    word2 = Convert.ToString(word2Box.Text);
+                    word3 = Convert.ToString(word3Box.Text);
+                    word4 = Convert.ToString(word4Box.Text);
+                    word5 = Convert.ToString(word5Box.Text);
+                    word6 = Convert.ToString(word6Box.Text);
+                    word7 = Convert.ToString(word7Box.Text);
+                    word8 = Convert.ToString(word8Box.Text);
+                    word9 = Convert.ToString(word9Box.Text);
+                    word10 = Convert.ToString(word10Box.Text);
+                }
+            }
+            else if(popUp == 1)
+            {
+                warningDown();
+                popUp = 0;
+                warningPopUp.Visible = false;
+                confirmButton.Text = "Confirm my Choices";
+                mainButton.Visible = true;
+                confirmButton.Location = new Point(confirmButton.Location.X - 77, confirmButton.Location.Y);
+            }
         }
     }
 }
