67e2101d352ece628a08c325450e985825b99512
BuildTools <unconfigured@null.spigotmc.org>
2020-06-18 00:32:09 -0400

    okay i did it

--- MadLibs/Form1.cs
+++ MadLibs/Form1.cs
@@ -27,6 +27,7 @@ namespace MadLibs
 
         public void titleSetup()
         {
+            // make the title look like a title by doing everything below.
             titleSmile.Visible = true;
             titleLabel.Visible = true;
             titleLabel.Text = "Mad Libs";
@@ -40,6 +41,7 @@ namespace MadLibs
         }
         public void titleClear()
         {
+            // clears the title so it doesnt exist to the user anymore, out of sight out of mind you know?
             titleSmile.Visible = false;
             titleLabel.Visible = false;
             subtitleLabel.Visible = false;
@@ -52,6 +54,7 @@ namespace MadLibs
         }
         public void selectSetup()
         {
+            // lets you choose the bundle of joy that you are about the experience.
             titleLabel.Visible = true;
             titleLabel.Text = "Stages";
             turtleButton.Visible = true;
@@ -64,6 +67,7 @@ namespace MadLibs
         }
         public void selectClear()
         {
+            // the bundle of joy has been chosen or disregarded and is now gone... you are left to what you have.
             titleLabel.Visible = false;
             titleLabel.Text = "Mad Libs";
             turtleButton.Visible = false;
@@ -76,6 +80,7 @@ namespace MadLibs
         }
         public void wordSetup()
         {
+            // you now have basically complete control of the world you form around you, what choices will you make?
             mainButton.Visible = true;
             confirmButton.Visible = true;
             confirmButton.Enabled = true;
@@ -103,6 +108,7 @@ namespace MadLibs
         }
         public void wordClear()
         {
+            // your choices have been made or disregarded, you will face the consequences of your actions.
             mainButton.Visible = false;
             confirmButton.Visible = false;
             confirmButton.Enabled = false;
@@ -140,6 +146,7 @@ namespace MadLibs
         }
         public void warningUp()
         {
+            // you have made a fool of yourself, you can never turn back, well thats not true you can pretend you cant for the dramatic effect!
             word1Label.Visible = false;
             word1Box.Visible = false;
             word2Label.Visible = false;
@@ -163,6 +170,7 @@ namespace MadLibs
         }
         public void warningDown()
         {
+            // this is irrelevant but regardless if you see this on your screen doom has come to you and you must run, run like your life depends on it.
             word1Label.Visible = true;
             word1Box.Visible = true;
             word2Label.Visible = true;
@@ -188,6 +196,7 @@ namespace MadLibs
         
         public void turtleSetup()
         {
+            // you have chosen the turtle story, do what you will with it.
             story = 1;
             word1Label.Text = "Adjective:";
             word2Label.Text = "Adjective:";
@@ -202,6 +211,7 @@ namespace MadLibs
         }
         public void turtleStory()
         {
+            // what have you done the story is insane your choices their all there, oh my lord!
             storyPopUp.Text = "The " + word1 + " " + word2 + " " + word3 + " " + word4 + " are 4 " + word4 + " who are led ";
             storyPopUp.Text += "by a/an " + word5 + " in the fight against " + word6 + ". They strive to protect " + word7 + " from all ";
             storyPopUp.Text += "violence, but their most " + word8 + " enemy is the dreaded " + word9 + ". They also love ";
@@ -210,6 +220,7 @@ namespace MadLibs
 
         public void loveSetup()
         {
+            // maybe in this story you will experience joy and love, this is dependent on the choices you make. 
             story = 2;
             word1Label.Text = "Famous Name:";
             word2Label.Text = "Adjective:";
@@ -224,6 +235,7 @@ namespace MadLibs
         }
         public void loveStory()
         {
+            // you wrote a letter to your one to be, will you get a response, i highly doubt it
             storyPopUp.Text = "Dear " + word1 + ",";
             storyPopUp.Text += "\n\nYou are extremely " + word2 + " and I " + word3 + " you! I want to kiss your " + word4 + " " + word5;
             storyPopUp.Text += " times. You make my " + word6 + " burn with desire. When I first saw you, I " + word7 + " stared at you and ";
@@ -234,6 +246,7 @@ namespace MadLibs
 
         public void timeSetup()
         {
+            //time is running out read this fast and give us 100%
             story = 3;
             word1Label.Text = "Noun:";
             word2Label.Text = "Famous Name:";
@@ -248,6 +261,7 @@ namespace MadLibs
         }
         public void timeStory()
         {
+            // fight and thrive, will you survive the day, that depends on your choices
             storyPopUp.Text = "Once upon a time, there was a " + word1 + " called " + word2 + ". Their parents would describe ";
             storyPopUp.Text += "them as " + word3 + ", but all of their friends thought they were really " + word4 + ". ";
             storyPopUp.Text += "One day, a " + word5 + " " + word6 + " came to " + word2 + " and said, 'I hate ";
@@ -258,6 +272,7 @@ namespace MadLibs
 
         public void fortuneSetup()
         {
+            // Money money money make the right choices
             story = 4;
             word1Label.Text = "Verb:";
             word2Label.Text = "Adverb:";
@@ -272,6 +287,7 @@ namespace MadLibs
         }
         public void fortuneStory()
         {
+            // This cookie holds your life! it has immense power! be afraid of it!
             storyPopUp.Text = "~~" + word1 + " " + word2 + " without being " + word3 + ".~~";
             storyPopUp.Text += "\n\n~~The universe does not have " + word4 + " -- it has~~";
             storyPopUp.Text += "\n~~" + word5 + ", and " + word5 + " can be " + word6 + ".~~";
@@ -284,6 +300,7 @@ namespace MadLibs
 
         public void BlankSetup()
         {
+            // Blank ball
             story = 5;
             word1Label.Text = "Noun:";
             word2Label.Text = "Verb (Ending in 'ing'):";
@@ -298,6 +315,7 @@ namespace MadLibs
         }
         public void BlankStory()
         {
+            // SCOOOOOORE did you make it?
             storyPopUp.Text = word1 + "ball is a very exciting sport. Whether you are " + word2 + " the ball with a ";
             storyPopUp.Text += word3 + ", or you are the " + word4 + " and striking out " + word5 + " batters in a row, you'll be having ";
             storyPopUp.Text += "fun. You can also be the catcher, standing at " + word6 + " plate, ready to " + word7 + " the next person ";
@@ -308,6 +326,7 @@ namespace MadLibs
 
         private void randButton_Click(object sender, EventArgs e)
         {
+            // chooses your destiny for you, nerd.
             titleClear();
 
             story = randGen.Next(1, 6);
@@ -346,30 +365,35 @@ namespace MadLibs
 
         private void turtleButton_Click_1(object sender, EventArgs e)
         {
+            // you have chosen the turtle demise
             selectClear();
             wordSetup();
             turtleSetup();
         }
         private void loveButton_Click(object sender, EventArgs e)
         {
+            // they dont love you stop trying, also it means you chose the love story
             selectClear();
             wordSetup();
             loveSetup();
         }
         private void timeButton_Click(object sender, EventArgs e)
         {
+            // the world needs you, wake up! also it means you chose the time story
             selectClear();
             wordSetup();
             timeSetup();
         }
         private void fortuneButton_Click(object sender, EventArgs e)
         {
+            // this cookie cant wake you up keep trying! also it means you chose the fortune story
             selectClear();
             wordSetup();
             fortuneSetup();
         }
         private void blankButton_Click_1(object sender, EventArgs e)
         {
+            // Hit the ball all you want, wake up! also it means you chose the blankball story
             selectClear();
             wordSetup();
             BlankSetup();
@@ -378,15 +402,18 @@ namespace MadLibs
 
         private void selectButton_Click(object sender, EventArgs e)
         {
+            // you choose your fate, choose wisely or else you may regret it
             titleClear();
             selectSetup();
         }
         private void quitButton_Click(object sender, EventArgs e)
         {
+            // a wise choice, do not dabble in the magics of fate
             Environment.Exit(1);
         }
         private void mainButton_Click(object sender, EventArgs e)
         {
+            // you may be rethinking your decisions? this brings you back to your main menu gateway
             selectClear();
             titleSetup();
             warningDown();
@@ -397,6 +424,7 @@ namespace MadLibs
 
         private void confirmButton_Click(object sender, EventArgs e)
         {
+            // your choices are confirmed, do you regret them? you can still go back.
             if (popUp == 0)
             {
                 if (word1Box.Text == null || word1Box.Text == "" || word2Box.Text == null || word2Box.Text == "" || word3Box.Text == null || word3Box.Text == "" || word4Box.Text == null || word4Box.Text == "" || word5Box.Text == null || word5Box.Text == "" || word6Box.Text == null || word6Box.Text == "" || word7Box.Text == null || word7Box.Text == "" || word8Box.Text == null || word8Box.Text == "" || word9Box.Text == null || word9Box.Text == "" || word10Box.Text == null || word10Box.Text == "")
